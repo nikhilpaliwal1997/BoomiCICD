3c7af235934e0ddce3c153fb6375545b2a7eda29
Nikhil Paliwal <42150617+nikhilpaliwal1997@users.noreply.github.com>
2026-04-03 14:49:47 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,4 @@
 curl -X POST 
 https://api.boomi.com/api/rest/v1/$BOOMI_ACCOUNT_ID/PackagedComponent -u $BOOMI_USERNAME:$BOOMI_PASSWORD -H "Content-Type: application/json" -d '{ "componentId": "'$BOOMI_COMPONENT_ID'", "notes": "CI/CD Deployment" }'
+curl -X POST 
+https://api.boomi.com/api/rest/v1/$BOOMI_ACCOUNT_ID/DeployedPackage -u $BOOMI_USERNAME:$BOOMI_PASSWORD -H "Content-Type: application/json" -d '{ "environmentId": "'$BOOMI_ENVIRONMENT_ID'", "notes": "Automated Deployment" }
